be76fb049eb2da6bb295fedf669624594afeb439
Minseo Jeon <minseo4565@snu.ac.kr>
2025-08-08 02:08:35 +0900

    restart, deathBehaviour

--- Assets/Scripts/EntityControl/PlayerController.cs
+++ Assets/Scripts/EntityControl/PlayerController.cs
@@ -3,6 +3,7 @@ using PlayerControl;
 using StateMachine;
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : EntityController
 {
@@ -52,9 +53,7 @@ public class PlayerController : EntityController
 
         if (hp < 0)
         {
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
-            Time.timeScale = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
 
         mp += Time.deltaTime * 3f;
