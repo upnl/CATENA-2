e4f774310dcf4f6ca33a549f5d076457edc3190f
sonojae <sson41541241@gmail.com>
2025-08-07 20:20:24 +0900

    [feat] Setting CH2 Skill2

---
+++ Assets/Scripts/StateMachine/SkillState/Character2Skill2State.cs
@@ -0,0 +1,69 @@
+﻿using PlayerControl;
+using UnityEngine;
+using StateMachine;
+using UnityEngine.InputSystem;
+
+public class Character2Skill2State : EntitySkillState
+{
+    private Rigidbody _rigidbody;
+    private PlayerController _playerController;
+    
+    private Transform _playerTransform;
+
+    private bool _isMoving;
+    private float _chargingTime;
+    private Vector3 _dashDir;
+    
+    public Character2Skill2State(EntityStateMachine entityStateMachine) : base(entityStateMachine)
+    {
+        _rigidbody = stateMachine.EntityController.GetComponent<Rigidbody>();
+        _playerController = stateMachine.EntityController as PlayerController;
+
+        _playerTransform = _playerController.transform;
+
+        AttackContext = _playerController.attackContextSO.contexts[5];
+    }
+    
+    public override void Enter()
+    {
+        //TODO : skill2
+        base.Enter();
+        
+        stateMachine.PlayAnimation("Skill2");
+        _chargingTime = 0f;
+        AttackContext = _playerController.attackContextSO.contexts[5];
+        
+    }
+
+    public override void Update()
+    {
+        
+        base.Update();
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+
+        if (_isMoving)
+        {
+            _rigidbody.MovePosition(stateMachine.EntityController.transform.position +
+                                    _dashDir * (AttackContext.floatVariables[0] * Time.fixedDeltaTime));
+        }
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        
+        // _playerController.RemoveActionTrigger(ActionTriggerType.Skill, );
+    }
+    
+    private void OnDodge(ActionTriggerContext context)
+    {
+        if (context.InputActionPhase == InputActionPhase.Performed)
+        {
+            stateMachine.ChangeState(stateMachine.EntityDodgeState);
+        }
+    }
+}
