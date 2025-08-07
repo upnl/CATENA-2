11a7ef0ef912a04b10b3c8f750e9142c06690c77
sonojae <sson41541241@gmail.com>
2025-08-07 18:07:38 +0900

    [feat] StateMachining ch3NormalAttack

---
+++ Assets/Scripts/EntityControl/Character3Controller.cs
@@ -0,0 +1,11 @@
+﻿using StateMachine;
+
+public class Character3Controller : PlayerController
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        
+        StateMachine = new Character3StateMachine(this);
+    }
+}
