c54972eb00be47ea0cbb6dd6074a3567db106189
sonojae <sson41541241@gmail.com>
2025-08-07 15:58:40 +0900

    [feat] StateMachining Character2 A/AB

---
+++ Assets/Scripts/StateMachine/Character2NormalAttackState.cs
@@ -0,0 +1,12 @@
+﻿using PlayerControl;
+using UnityEngine;
+using StateMachine;
+using UnityEngine.InputSystem;
+
+public class Character2NormalAttackState : EntityNormalAttackState
+{
+    public Character2NormalAttackState(EntityStateMachine entityStateMachine) : base(entityStateMachine)
+    {
+        NormalAttackStateMachine = new Character2NormalAttackSM(stateMachine.EntityController, stateMachine);
+    }
+}
---
+++ Assets/Scripts/StateMachine/Character2StateMachine.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace StateMachine
+{
+    public class Character2StateMachine : PlayerStateMachine
+    {
+        public Character2StateMachine(EntityController entityController) : base(entityController)
+        {
+            EntityNormalAttackState = new Character2NormalAttackState(this);
+        }
+    }
+}
